0227755510292a38f285efd844ac05f4647f3439
Andre Carstens <andrejcarstens@gmail.com>
2023-09-25 20:59:30 +0200

    compose changes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using NN.Cart.data;
 using NN.Cart.services;
 using NN.Cart.services.interfaces;
@@ -10,6 +11,30 @@ namespace NN.Cart
     {
         public static void Main(string[] args)
         {
+            // Enable the selflog output
+            SelfLog.Enable(Console.Error);
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(theme: SystemConsoleTheme.Literate)
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(Configuration.GetConnectionString("elasticsearch"))) // for the docker-compose implementation
+                {
+                    AutoRegisterTemplate = true,
+                    OverwriteTemplate = true,
+                    DetectElasticsearchVersion = true,
+                    AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                    NumberOfReplicas = 1,
+                    NumberOfShards = 2,
+                    //BufferBaseFilename = "./buffer",
+                    // RegisterTemplateFailure = RegisterTemplateRecovery.FailSink,
+                    FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+                    EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                       EmitEventFailureHandling.WriteToFailureSink |
+                                       EmitEventFailureHandling.RaiseCallback,
+                    FailureSink = new FileSink("./fail-{Date}.txt", new JsonFormatter(), null, null)
+                })
+                .CreateLogger();
+
+            Log.Information("Hello, world!");
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
